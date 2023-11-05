ddd73dacb2bb273cbd61014f613206e013c7a72c
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2023-11-05 11:08:36 -0600

    Added Box animation

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,8 +1,22 @@
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+
 const Box = () => {
+  const boxVariant = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  };
+
   return (
-    <div className="box">
+    <motion.div
+    animate={{ x: 100 }}
+    initial={{x: 0}}
+    variants={boxVariant}
+    className="box"
+    >
       <h1>Hello Jerks</h1>
-    </div>
+    </motion.div>
   );
 };
 
